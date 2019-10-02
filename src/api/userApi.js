d6d04b678ccc7b6748f55dbd95c50a4b430bf14f
Eric Warren <emw-ghertner@users.noreply.github.com>
2019-10-02 08:00:33 -0500

    userApi.js fixed

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,3 +1,15 @@
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
+
+export function getUsers() {
+  return get('users');
+}
+
+function get(url) {
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
 function onSuccess(response) {
   return response.json();
 }
@@ -6,13 +18,7 @@ function onError(error) {
   console.log(error); // eslint-disable-line no-console
 }
 
-function get(url) {
-  return fetch(url).then(onSuccess, onError);
-}
 
-export function getUsers() {
-  return get('users');
-}
 
 
 
