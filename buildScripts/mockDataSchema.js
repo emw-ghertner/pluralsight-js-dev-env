f074f43a341533d29d2dc3bbff0012b41abd3267
Eric Warren <emw-ghertner@users.noreply.github.com>
2019-10-02 08:31:03 -0500

    fixed id generation

--- buildScripts/mockDataSchema.js
+++ buildScripts/mockDataSchema.js
@@ -1,33 +1,34 @@
 export const schema = {
-  "type": "object",
-  "properties": {
-    "users": {
-      "type": "array",
-      "minItems": 3,
-      "maxItems": 5,
-      "items": {
-        "type": "object",
-        "properties": {
-          "id": {
-            "type": "integer"
+  'type': 'object',
+  'properties': {
+    'users': {
+      'type': 'array',
+      'minItems': 3,
+      'maxItems': 5,
+      'items': {
+        'type': 'object',
+        'properties': {
+          'id': {
+            'type': 'integer',
+            'faker': 'finance.account',
           },
-          "firstName": {
-            "type": "string",
-            "faker": "name.firstName"
+          'firstName': {
+            'type': 'string',
+            'faker': 'name.firstName',
           },
-          "lastName": {
-            "type": "string",
-            "faker": "name.lastName"
+          'lastName': {
+            'type': 'string',
+            'faker': 'name.lastName',
           },
-          "email": {
-            "type": "string",
-            "format": "email",
-            "faker": "internet.email"
+          'email': {
+            'type': 'string',
+            'format': 'email',
+            'faker': 'internet.email',
           }
         },
-        "required": ["id", "firstName", "lastName", "email"]
+        'required': ['id', 'firstName', 'lastName', 'email']
       }
     }
   },
-  "required": ["users"]
+  'required': ['users']
 };
